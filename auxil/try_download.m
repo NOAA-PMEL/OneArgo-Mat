582ae304c271a62d1ea97d2c0eac1b2a4f4b18fa
Hartmut Frenzel <hartmut.frenzel@noaa.gov>
2023-06-12 17:53:00 -0700

    Delete a faulty netcdf file after unsuccessful download

--- auxil/try_download.m
+++ auxil/try_download.m
@@ -1,6 +1,6 @@
 function success = try_download(filename, dest_path)
 % try_download  This function is part of the
-% MATLAB toolbox for accessing BGC Argo float data.
+% MATLAB toolbox for accessing Argo float data.
 %
 % USAGE:
 %   success = try_download(filename, dest_path)
@@ -23,7 +23,7 @@ function success = try_download(filename, dest_path)
 % CITATION:
 %   H. Frenzel, J. Sharp, A. Fassbender, N. Buzby, 2022. OneArgo-Mat:
 %   A MATLAB toolbox for accessing and visualizing Argo data.
-%   Zenodo. https://doi.org/10.5281/zenodo.6588042
+%   Zenodo. https://doi.org/10.5281/zenodo.6588041
 %
 % LICENSE: oneargo_mat_license.m
 %
@@ -39,6 +39,11 @@ for h = 1:length(Settings.hosts)
                 [Settings.hosts{h}, filename], dest_path);
         end
         websave(dest_path, [Settings.hosts{h}, filename]);
+        [~,~,ext] = fileparts(dest_path);
+        % this will throw an exception if it is not a valid nc file:
+        if strcmp(ext, '.nc')
+            ncinfo(dest_path);
+        end
         if Settings.verbose
             fprintf('success!\n');
         end
@@ -52,6 +57,10 @@ for h = 1:length(Settings.hosts)
         % attempt
         if exist([dest_path, '.html'], 'file')
             delete([dest_path, '.html']);
+        end
+        % delete broken netcdf file if it was downloaded
+        if exist(dest_path, 'file')
+            delete(dest_path);
         end
         if h == length(Settings.hosts)
             if exist(dest_path, 'file') ~= 2
